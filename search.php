84fdd4116c0ed65f7f9fffcdbed3061cd7507e70
Joe Steinkamp <joe@joesteinkamp.com>
2016-11-20 16:32:34 -0600

    Added search form to the search results page.

--- search.php
+++ search.php
@@ -18,6 +18,8 @@ get_header(); ?>
 				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentysixteen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
 			</header><!-- .page-header -->
 
+			<?php get_search_form() ?>
+
 			<?php
 			// Start the loop.
 			while ( have_posts() ) : the_post();
